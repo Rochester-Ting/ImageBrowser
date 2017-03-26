a1525ffee70e25dd4419c2524088153c1a60f3db
Rochester.Ting <694526771@qq.com>
2017-03-26 16:56:56 +0800

    修复显示bug

--- ImageBrowser/ImageBrowser/Class - /Home - /Controller/DetailVC.swift
+++ ImageBrowser/ImageBrowser/Class - /Home - /Controller/DetailVC.swift
@@ -45,6 +45,7 @@ extension DetailVC{
         collectionView.dataSource = self
         
         collectionView.delegate = self
+        collectionView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 10)
 
     }
 }
